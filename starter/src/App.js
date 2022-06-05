d9d0338140a3ff573976d26483a6c001bb29ba8e
amanialosaimi <amanialossaimi@gmail.com>
2022-06-05 20:34:36 +0300

    Search component

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,32 +1,13 @@
 import "./App.css";
 import { useState } from "react";
+import Search from "./Search";
 
 function App() {
   const [showSearchPage, setShowSearchpage] = useState(false);
 
   return (
     <div className="app">
-      {showSearchPage ? (
-        <div className="search-books">
-          <div className="search-books-bar">
-            <a
-              className="close-search"
-              onClick={() => setShowSearchpage(!showSearchPage)}
-            >
-              Close
-            </a>
-            <div className="search-books-input-wrapper">
-              <input
-                type="text"
-                placeholder="Search by title, author, or ISBN"
-              />
-            </div>
-          </div>
-          <div className="search-books-results">
-            <ol className="books-grid"></ol>
-          </div>
-        </div>
-      ) : (
+        <Search/>
         <div className="list-books">
           <div className="list-books-title">
             <h1>MyReads</h1>
@@ -276,7 +257,6 @@ function App() {
             <a onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</a>
           </div>
         </div>
-      )}
     </div>
   );
 }
---
+++ starter/src/Search.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import { useState } from "react";
+
+const Search = () => {
+  const [showSearchPage, setShowSearchpage] = useState(false);
+
+  return(
+    <div className="search-books">
+    <div className="search-books-bar">
+      <a
+        className="close-search"
+        onClick={() => setShowSearchpage(!showSearchPage)}
+      >
+        Close
+      </a>
+      <div className="search-books-input-wrapper">
+        <input
+          type="text"
+          placeholder="Search by title, author, or ISBN"
+        />
+      </div>
+    </div>
+    <div className="search-books-results">
+      <ol className="books-grid"></ol>
+    </div>
+    </div>
+  )
+
+}
+
+export default Search
+
